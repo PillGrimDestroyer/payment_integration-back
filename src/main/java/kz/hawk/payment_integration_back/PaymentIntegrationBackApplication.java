6eac9ddc45cf876bdd5c7e4972c87669998e0d17
WorkHawk <megamega4@yandex.ru>
2026-02-18 09:19:23 +0500

    Init

---
+++ src/main/java/kz/hawk/payment_integration_back/PaymentIntegrationBackApplication.java
@@ -0,0 +1,13 @@
+package kz.hawk.payment_integration_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentIntegrationBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PaymentIntegrationBackApplication.class, args);
+  }
+
+}
---
+++ src/test/java/kz/hawk/payment_integration_back/PaymentIntegrationBackApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.hawk.payment_integration_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentIntegrationBackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
