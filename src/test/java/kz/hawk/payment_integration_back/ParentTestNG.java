91463ab715fbf5abff56fc3b129a08819490f934
WorkHawk <megamega4@yandex.ru>
2026-02-18 12:33:46 +0500

    Switch to TestNG for testing; update dependencies and introduce base test class

--- src/test/java/kz/hawk/payment_integration_back/ParentTestNG.java
+++ src/test/java/kz/hawk/payment_integration_back/ParentTestNG.java
@@ -2,9 +2,11 @@ package kz.hawk.payment_integration_back;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 
 @SpringBootTest
-class PaymentIntegrationBackApplicationTests {
+@SuppressWarnings("NewClassNamingConvention")
+public class ParentTestNG extends AbstractTestNGSpringContextTests {
 
   @Test
   void contextLoads() {
